d61b0e11a330ad6c544fba0acca524717b972761
mgshow <g.maurizio@lemansys.com>
2014-03-03 00:24:34 +0100

    Examples tests, collections

---
+++ XLEWS_SERVER/xlem/data/collections.py
@@ -0,0 +1,34 @@
+'''
+Created on Mar 2, 2014
+
+@author: mgshow
+'''
+
+class XLemVector(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params=None):
+        '''
+        Constructor
+        '''
+        __values=[]
+        
+    def size(self):
+        return len(self.__values)
+    
+    def add(self, o):
+        self.__values.append(o)
+
+class XLemMap(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params=None):
+        '''
+        Constructor
+        '''
---
+++ XLEWS_SERVER/xlem/data/dateAndTime.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 2, 2014
+
+@author: mgshow
+'''
+
+class XLemDate(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params=None):
+        '''
+        Constructor
+        '''
+        
